37ddc55fa0d0d7265fd8725af8c05f86c29921c7
Grégory Planchat <gregory@kiboko.fr>
2020-01-03 11:56:37 +0100

    Changed the presenter service in the FastMap extension

--- src/FastMap/FastMapExtension.php
+++ src/FastMap/FastMapExtension.php
@@ -12,15 +12,15 @@ final class FastMapExtension implements Extension
     public function load(ServiceContainer $container, array $params)
     {
         $container->define('matchers.execute_compiled_mapping', function (IndexedServiceContainer $c) {
-            return new Matcher\ExecuteCompiledMapping($c->get('formatter.presenter.value_presenter'));
+            return new Matcher\ExecuteCompiledMapping($c->get('formatter.presenter'));
         }, ['matchers']);
 
         $container->define('matchers.execute_uncompiled_mapping', function (IndexedServiceContainer $c) {
-            return new Matcher\ExecuteUncompiledMapping($c->get('formatter.presenter.value_presenter'));
+            return new Matcher\ExecuteUncompiledMapping($c->get('formatter.presenter'));
         }, ['matchers']);
 
         $container->define('matchers.throw_when_execute_uncompiled_mapping', function (IndexedServiceContainer $c) {
-            return new Matcher\ThrowWhenExecuteCompiledMappingMatcher($c->get('unwrapper'), $c->get('formatter.presenter.value_presenter'), new ReflectionFactory());
+            return new Matcher\ThrowWhenExecuteCompiledMappingMatcher($c->get('unwrapper'), $c->get('formatter.presenter'), new ReflectionFactory());
         }, ['matchers']);
     }
 }
